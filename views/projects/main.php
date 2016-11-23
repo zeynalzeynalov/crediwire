4a9ef70787b05666b6e8b06e702aa7bf3313139e
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-23 21:29:27 +0400

    Update main.php

--- views/projects/main.php
+++ views/projects/main.php
@@ -12,18 +12,22 @@
     
     ?>
 
-    <input class="btn btn-<?= $project->Project_State == "Start working"? "success" : "danger" ?>" type="button" id="btnAjax_<?= $project->Project_ID ?>" value="<?= $project->Project_State ?>" onclick="showHint(this.id, 'getProjectTimeRecords')">
+
     
-    
-    
-    <a class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+    <a class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseExample_<?= $project->Project_ID ?>" aria-expanded="false" aria-controls="collapseExample">
  Show/Hide working details:
 </a>
-<div class="collapse" id="collapseExample">
+<div class="collapse" id="collapseExample_<?= $project->Project_ID ?>">
   <div class="well">
     <p> <span id="txtHint<?= $project->Project_ID ?>"></span></p>
   </div>
 </div>
+    
+    
+        <input class="btn btn-<?= $project->Project_State == "Start working"? "success" : "danger" ?>" type="button" id="btnAjax_<?= $project->Project_ID ?>" value="<?= $project->Project_State ?>" onclick="showHint(this.id, 'getProjectTimeRecords')">
+    
+    
+    
     
   </div>
 </div>
