f7a42b70c3b0e82633ca915f26fe8602c66605ae
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-23 16:26:16 +0400

    Create project.php

---
+++ models/project.php
@@ -0,0 +1,36 @@
+<?php
+  class Project {
+    // we define 3 attributes
+    // they are public so that we can access them using $post->author directly
+    public $id;
+
+    public function __construct($id) {
+      $this->id      = $id;
+    }
+
+    public static function all() {
+      $list = [];
+      $db = Db::getInstance();
+      $req = $db->query('SELECT * FROM posts');
+
+      // we create a list of Post objects from the database results
+      foreach($req->fetchAll() as $post) {
+        $list[] = new Post($post['id'], $post['author'], $post['content']);
+      }
+
+      return $list;
+    }
+
+    public static function find($id) {
+      $db = Db::getInstance();
+      // we make sure $id is an integer
+      $id = intval($id);
+      $req = $db->prepare('SELECT * FROM posts WHERE id = :id');
+      // the query was prepared, now we replace :id with our actual $id value
+      $req->execute(array('id' => $id));
+      $post = $req->fetch();
+
+      return new Post($post['id'], $post['author'], $post['content']);
+    }
+  }
+?>
