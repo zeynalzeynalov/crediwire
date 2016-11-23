8b29825b840597f99a68b3c927009430b52f3dff
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-23 16:31:48 +0400

    Update template.php

--- views/template.php
+++ views/template.php
@@ -7,7 +7,7 @@
       <a href='/php_mvc_blog'>Home</a>
     </header>
 
-    <?php require_once(dirname(dirname(__FILE__)).'includes/starter.php'); ?>
+    <?php require_once(dirname(dirname(__FILE__)).'/includes/starter.php'); ?>
 
     <footer>
       Copyright
