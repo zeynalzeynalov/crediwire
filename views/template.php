d67acd7b5494f016d49546526cfa9d06d730d043
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-23 18:57:53 +0400

    Update template.php

--- views/template.php
+++ views/template.php
@@ -10,6 +10,25 @@
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+    <script>
+    function showHint(str) {
+    if (str.length == 0) { 
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    } else {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("txtHint").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET", "recordprojecttime.php?project_id=" + str, true);
+        xmlhttp.send();
+    }
+    }
+    </script>
+    
+    
   </head>
   <body>
     
