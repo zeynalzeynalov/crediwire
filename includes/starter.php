f0bf0cfe91740e5a34aaab259c48d0e7d0178784
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-23 16:21:17 +0400

    Update starter.php

--- includes/starter.php
+++ includes/starter.php
@@ -1,32 +1,32 @@
 <?php
   function call($controller, $action) {
-    // require the file that matches the controller name
     require_once('controllers/' . $controller . '_controller.php');
 
-    // create a new instance of the needed controller
     switch($controller) {
       case 'pages':
-        $controller = new PagesController();
+        $controller = new ProjectsController();
+      break;
+      case 'posts':
+        // we need the model to query the database later in the controller
+        require_once('models/post.php');
+        $controller = new PostsController();
       break;
     }
 
-    // call the action
     $controller->{ $action }();
   }
 
-  // just a list of the controllers we have and their actions
-  // we consider those "allowed" values
-  $controllers = array('pages' => ['home', 'error']);
+  // we're adding an entry for the new controller and its actions
+  $controllers = array('projects' => ['main', 'error'],
+                       'posts' => ['index', 'show']);
 
-  // check that the requested controller and action are both allowed
-  // if someone tries to access something else he will be redirected to the error action of the pages controller
   if (array_key_exists($controller, $controllers)) {
     if (in_array($action, $controllers[$controller])) {
       call($controller, $action);
     } else {
-      call('pages', 'error');
+      call('projects', 'error');
     }
   } else {
-    call('pages', 'error');
+    call('projects', 'error');
   }
 ?>
