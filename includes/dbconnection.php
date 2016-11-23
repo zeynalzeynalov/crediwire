f9cf7e82252af0745b23528b927da1ce2b194168
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-23 17:14:15 +0400

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -5,7 +5,7 @@
  
     function connectToDB()
     {    
-        $dbConnection = pg_connect(sprintf("host=%s user=%s password=%s dbname=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);)
+        $dbConnection = pg_connect(sprintf("host=%s user=%s password=%s dbname=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME))
           
         or die ("Could not connect to server\n");;
          
