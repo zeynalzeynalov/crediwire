a1551fdab91b2b751ea8c2a2a9b4f0b112555ef5
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-23 16:56:54 +0400

    Update main.php

--- views/projects/main.php
+++ views/projects/main.php
@@ -1,3 +1,5 @@
-<p>Hello there <?php echo $first_name . ' ' . $last_name; ?>!<p>
+<p>Hello there <?php echo $projects[0]['Project_ID']; ?>!<p>
 
+    
+  
 <p>You successfully landed on the main page. Congrats!</p>
