5adab1bdde1aced29195882d7d85293ae18ced5c
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-23 18:09:02 +0400

    Update main.php

--- views/projects/main.php
+++ views/projects/main.php
@@ -1,6 +1,12 @@
-<p>List of projects (main page):
-    <?php
-    foreach ($projects as $project)
-        echo 'Project: ' . $project->Project_ID . ' : ' . $project->Project_Title . ' : ' . $project->Project_Slug . ' : ' . $project->Project_Created_Date . "\n";
-    ?>
-<p>
+<?php foreach ($projects as $project): ?>
+
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title"><?= $project->Project_Title ?></h3>
+  </div>
+  <div class="panel-body">
+    <?= 'Project: ' . $project->Project_ID . ' : ' . $project->Project_Title . ' : ' . $project->Project_Slug . ' : ' . $project->Project_Created_Date
+  </div>
+</div>
+      
+<?php endforeach; ?>
