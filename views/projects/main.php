4f911731862a854a4d73011fe1c6d14737191abe
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-23 17:40:17 +0400

    Update main.php

--- views/projects/main.php
+++ views/projects/main.php
@@ -1,10 +1,6 @@
-<p>List of projects:
+<p>List of projects (main page):
     <?php
     foreach ($projects as $project)
-        echo 'Project: ' . $project->Project_ID . ' : ' . $project->Project_Title . "\n";
+        echo 'Project: ' . $project->Project_ID . ' : ' . $project->Project_Title . ' : ' . $project->Project_Slug . ' : ' . $project->Project_Created_Date . "\n";
     ?>
 <p>
-
-    
-  
-<p>You successfully landed on the main page. Congrats!</p>
