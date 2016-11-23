d3b9d3196aed7120ff3c08599b6cff510403fc6d
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-23 16:49:18 +0400

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -3,9 +3,9 @@
   {
     protected static $dbConnection = NULL;
  
-    function connectToDB($host, $user, $password, $dbname)
-    {
-        $this->dbConnection = pg_connect("host=$host user=$user password=$password dbname=$dbname");
+    function connectToDB()
+    {    
+        $this->dbConnection = pg_connect("host=DB_HOST user=DB_USER password=DB_PASSWORD dbname=DB_NAME");
          
         return $this->dbConnection;
     }
