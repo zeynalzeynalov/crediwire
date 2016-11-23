52a19bd37e1b1acc3efcdad01b84680e90435424
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-23 18:10:48 +0400

    Update main.php

--- views/projects/main.php
+++ views/projects/main.php
@@ -5,7 +5,7 @@
     <h3 class="panel-title"><?= $project->Project_Title ?></h3>
   </div>
   <div class="panel-body">
-    <?= 'Project: ' . $project->Project_ID . ' : ' . $project->Project_Title . ' : ' . $project->Project_Slug . ' : ' . $project->Project_Created_Date
+    <?= 'Project: ' . $project->Project_ID . ' : ' . $project->Project_Title . ' : ' . $project->Project_Slug . ' : ' . $project->Project_Created_Date ?>
   </div>
 </div>
       
