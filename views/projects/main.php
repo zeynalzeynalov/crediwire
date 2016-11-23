7501a9adbc7d1225bb04673ab3bb4a917da8b1c7
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-23 21:27:49 +0400

    Update main.php

--- views/projects/main.php
+++ views/projects/main.php
@@ -14,7 +14,17 @@
 
     <input class="btn btn-<?= $project->Project_State == "Start working"? "success" : "danger" ?>" type="button" id="btnAjax_<?= $project->Project_ID ?>" value="<?= $project->Project_State ?>" onclick="showHint(this.id, 'getProjectTimeRecords')">
     
-    <p>Working details: <span id="txtHint<?= $project->Project_ID ?>"></span></p>
+    
+    
+    <a class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+ Show/Hide working details:
+</a>
+<div class="collapse" id="collapseExample">
+  <div class="well">
+    <p> <span id="txtHint<?= $project->Project_ID ?>"></span></p>
+  </div>
+</div>
+    
   </div>
 </div>
       
