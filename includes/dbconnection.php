89e52ba44ea24294e64744df74cf684a459aa6b0
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-23 17:04:02 +0400

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -5,9 +5,9 @@
  
     function connectToDB()
     {    
-        $this->dbConnection = pg_connect("host=DB_HOST user=DB_USER password=DB_PASSWORD dbname=DB_NAME");
+        $dbConnection = pg_connect("host=DB_HOST user=DB_USER password=DB_PASSWORD dbname=DB_NAME");
          
-        return $this->dbConnection;
+        return $dbConnection;
     }
   }
 ?>
