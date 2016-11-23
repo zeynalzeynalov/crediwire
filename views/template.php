0c396ddcc5a048b6200481b71d46d5fcbd3de0b7
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-23 16:17:23 +0400

    Create template.php

---
+++ views/template.php
@@ -0,0 +1,16 @@
+<DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <header>
+      <a href='/php_mvc_blog'>Home</a>
+    </header>
+
+    <?php require_once(dirname(dirname(__FILE__)).'includes/starter.php'); ?>
+
+    <footer>
+      Copyright
+    </footer>
+  <body>
+<html>
