5c9e4cca109226faca3b23f8a598b18b6a697a19
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-24 17:03:52 +0400

    Update template.php

--- views/template.php
+++ views/template.php
@@ -10,53 +10,7 @@
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-    <script>
-    function showHint(js_project_id,js_action)
-    {
-        var tmp_js_project_id = js_project_id.split('_');
-        js_project_id = tmp_js_project_id[1];
-
-        if (js_project_id.length == 0)
-        { 
-            document.getElementById("txtHint" + js_project_id).innerHTML = "";
-            return;
-        } 
-        else 
-        {
-            var xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange = function()
-            {
-                if (this.readyState == 4 && this.status == 200)
-                {
-                    var objArray = JSON.parse(this.responseText);
-                    var outputHtml = "";
-                    for(var i = 0; i < objArray.length; i++)
-                    {
-                        outputHtml += ('<span class="label label-info">' + objArray[i].Starting_Time_Stamp + ' - ' + objArray[i].End_Time_Stamps + '</span><br>');
-                    }
-
-                    document.getElementById("txtHint" + js_project_id).innerHTML = outputHtml;
-
-                    if(document.getElementById("btnAjax_" + js_project_id).value == "Start working")
-                    {
-                        document.getElementById("btnAjax_" + js_project_id).value = "Stop working";
-                        document.getElementById("btnAjax_" + js_project_id).className = "btn btn-danger";
-                    }
-                    else
-                    {
-                        document.getElementById("btnAjax_" + js_project_id).value = "Start working";
-                        document.getElementById("btnAjax_" + js_project_id).className = "btn btn-success"; 
-                    }
-                }
-            };
-
-            xmlhttp.open("GET", "restfulapi/getjson.php/getProjectTimeRecords/" + js_project_id, true);
-            xmlhttp.send();
-        }
-    }
-    </script>
-    
-    
+    <script type="text/javascript" src="js/mainscript.js"></script>
   </head>
   <body>
     
