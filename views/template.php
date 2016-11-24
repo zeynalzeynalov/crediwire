e524a5517e9549fcd53828fc287861766496028e
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-24 17:13:48 +0400

    Update template.php

--- views/template.php
+++ views/template.php
@@ -42,7 +42,7 @@
 
           <ul class="nav navbar-nav navbar-right">
             <li><a href="#">Help</a></li>
-            <li><span class="label label-info">Welcome, Bob!</span></li>
+            <li>Welcome, <b>Bob!</b></li>
           </ul>
         </div><!-- /.navbar-collapse -->
       </div><!-- /.container-fluid -->
