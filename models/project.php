8bb927f217974bebf1a427eedd8bb24031db3b02
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-24 19:43:12 +0400

    Update project.php

--- models/project.php
+++ models/project.php
@@ -3,45 +3,29 @@
 
     public $Project_ID;
     public $Project_Title;
-    public $Project_Slug;
     public $Project_Created_Date;
     public $Project_State;
 
-    public function __construct($Project_ID,$Project_Title,$Project_Slug,$Project_Created_Date,$Project_State)
+    public function __construct($Project_ID, $Project_Title, $Project_Created_Date, $Project_State)
     {
         $this->Project_ID = $Project_ID;
         $this->Project_Title = $Project_Title;
-        $this->Project_Slug = $Project_Slug;
         $this->Project_Created_Date = $Project_Created_Date;
         $this->Project_State = $Project_State;
     }
 
-    public static function all()
+    public static function fetchAll()
     {
         $projectList = [];
-        $con = dbConnection::connectToDB();
-        $results = pg_query($con,'SELECT P.*, coalesce(TMP."Project_Open_Record_Count",0) "Project_Open_Record_Count"
-            FROM public."Project" P LEFT JOIN
-            (SELECT "Project_ID", count(*) "Project_Open_Record_Count" FROM public."Project_Execution_Record" WHERE "Is_Completed" = FALSE GROUP BY "Project_ID") TMP 
-            ON P."Project_ID" = TMP."Project_ID" 
-            ORDER BY P."Project_ID" ASC') or die('Query failed: ' . pg_last_error());
+        $dbCon = dbConnection::connectToDB();
+        $results = pg_query($dbCon,'SELECT P.*, public.Check_Project_State(1) "Project_State"
+            FROM public."Project" ORDER BY P."Project_ID" ASC') or die('Query failed: ' . pg_last_error());
 
         while ($row = pg_fetch_assoc($results))
-          $projectList[] = new Project($row['Project_ID'], $row['Project_Title'], $row['Project_Slug'], $row['Project_Created_Date'],$row['Project_Open_Record_Count'] > 0 ? "Stop working" : "Start working");
+          $projectList[] = new Project($row['Project_ID'], $row['Project_Title'], $row['Project_Created_Date'], $row['Project_State']);
 
+        pg_close($dbCon);
         return $projectList;
-    }
-
-    public static function find($id) {
-      $db = Db::getInstance();
-      // we make sure $id is an integer
-      $id = intval($id);
-      $req = $db->prepare('SELECT * FROM posts WHERE id = :id');
-      // the query was prepared, now we replace :id with our actual $id value
-      $req->execute(array('id' => $id));
-      $post = $req->fetch();
-
-      return new Post($post['id'], $post['author'], $post['content']);
     }
   }
 ?>
