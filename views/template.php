d7694d93e59c8b8a026c759795c34159f52c9676
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-24 21:16:13 +0400

    Update template.php

--- views/template.php
+++ views/template.php
@@ -32,7 +32,7 @@
           <ul class="nav navbar-nav">
             <!--li class="active"><a href="#">Dummy<span class="sr-only">(current)</span></a></li-->
             <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">API<span class="caret"></span></a>
+          <a href="#" class="btn btn-default dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">API<span class="caret"></span></a>
           <ul class="dropdown-menu">
             <li><a href="https://crediwire.herokuapp.com/restfulapi/getjson.php/getProjectDetails/1" target="_blank">API - Get Project Details (Project ID = 1)</a></li>
             <li><a href="https://crediwire.herokuapp.com/restfulapi/getjson.php/getProjectTimeRecords/1" target="_blank">API - Get Project Time Records (Project ID = 1)</a></li>
@@ -48,7 +48,7 @@
       </div><!-- /.container-fluid -->
     </nav>
     
-    <h4>Let's start completing waiting projects!</h4>
+    <center><h4>Let's start completing waiting projects!</h4></center>
     
     <div style="height: 50px;" />
     
