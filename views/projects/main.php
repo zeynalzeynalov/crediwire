be9b2de5884668618c8aa5f61782feaec802da9c
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-24 20:54:24 +0400

    Update main.php

--- views/projects/main.php
+++ views/projects/main.php
@@ -9,10 +9,12 @@
       <div class="panel-body">
       Project created on: <?= $project->Project_Created_Date ?>
 
-      <input class="<?= $project->getButtonStringCssClassForProjectState() ?>" type="button" id="btnAjax_<?= $project->Project_ID ?>" value="<?= $project->getButtonStringForProjectState() ?>" onclick="processAjaxRequest(this.id)">
-      <a class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseExample_<?= $project->Project_ID ?>" aria-expanded="false" aria-controls="collapseExample">
+      <a class="btn btn-default btn-sm" role="button" data-toggle="collapse" href="#collapseExample_<?= $project->Project_ID ?>" aria-expanded="false" aria-controls="collapseExample">
       Show/hide time records:
-      </a>
+      </a>  
+        
+      <input class="<?= $project->getButtonStringCssClassForProjectState() ?>" type="button" id="btnAjax_<?= $project->Project_ID ?>" value="<?= $project->getButtonStringForProjectState() ?>" onclick="processAjaxRequest(this.id)">
+
       <div class="collapse" id="collapseExample_<?= $project->Project_ID ?>">
       <div class="well">
       <p> <span id="ajaxResponseContainer<?= $project->Project_ID ?>"></span></p>
