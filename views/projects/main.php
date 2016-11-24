4cf7297b74731dceb10f31b317c8f6b91faf3c54
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-24 16:19:57 +0400

    Update main.php

--- views/projects/main.php
+++ views/projects/main.php
@@ -2,12 +2,10 @@
 
 <div class="panel panel-primary">
   <div class="panel-heading">
-    <h3 class="panel-title"><?= $project->Project_Title ?></h3>
+    <h3 class="panel-title">Project Name: <?= $project->Project_Title ?></h3>
   </div>
   <div class="panel-body">
-    <?= 
-    'Project: ' . $project->Project_ID . ' : ' . $project->Project_Title . ' : ' . $project->Project_Slug . ' : ' . $project->Project_Created_Date
-    ?>
+    Project created on: <?= $project->Project_Created_Date ?>
     
     <input class="btn btn-<?= $project->Project_State == "Start working"? "success" : "danger" ?>" type="button" id="btnAjax_<?= $project->Project_ID ?>" value="<?= $project->Project_State ?>" onclick="showHint(this.id, 'getProjectTimeRecords')">
     <a class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseExample_<?= $project->Project_ID ?>" aria-expanded="false" aria-controls="collapseExample">
