385501aa73dd586b34426cffbc60b9c7301362b3
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-24 19:50:39 +0400

    Update project.php

--- models/project.php
+++ models/project.php
@@ -23,8 +23,8 @@
     {
         $projectList = [];
         $dbCon = dbConnection::connectToDB();
-        $results = pg_query($dbCon,'SELECT P.*, public.Check_Project_State(1) "Project_State"
-            FROM public."Project" P ORDER BY P."Project_ID" ASC') or die('Query failed: ' . pg_last_error());
+        $results = pg_query($dbCon, 'SELECT P.*, public.Check_Project_State(P."Project_ID") "Project_State"
+            FROM public."Project" P ORDER BY P."Project_ID" ASC') or die('Select query failed: ' . pg_last_error());
 
         while ($row = pg_fetch_assoc($results))
           $projectList[] = new Project($row['Project_ID'], $row['Project_Title'], $row['Project_Created_Date'], $row['Project_State']);
