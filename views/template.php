658122b8de5ba8c00014cb08bbb69888edaf7925
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-24 21:01:53 +0400

    Update template.php

--- views/template.php
+++ views/template.php
@@ -61,7 +61,7 @@
     
 
     <footer>
-      CrediWire coding assignment. [Zeynal Zeynalov]
+      <center>CrediWire coding assignment. [Zeynal Zeynalov]</center>
     </footer>
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
