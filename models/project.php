e675d768e4a3e383ef12b8604b5b00020816b8a0
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-24 21:39:51 +0400

    Update project.php

--- models/project.php
+++ models/project.php
@@ -1,10 +1,23 @@
 <?php
-  class Project {
+
+    class Project_Execution_Record
+    {
+      public $Starting_Time_Stamp;
+      public $Ending_Time_Stamp;
+      public function __construct($Starting_Time_Stamp, $Ending_Time_Stamp)
+      {
+      $this->Starting_Time_Stamp = $Starting_Time_Stamp;
+      $this->Ending_Time_Stamp = $Ending_Time_Stamp;
+      }
+    }
+
+    class Project {
 
     public $Project_ID;
     public $Project_Title;
     public $Project_Created_Date;
     public $Project_State;
+    public $Project_Execution_Record;
 
     public function __construct($Project_ID, $Project_Title, $Project_Created_Date, $Project_State)
     {
@@ -12,6 +25,8 @@
         $this->Project_Title = $Project_Title;
         $this->Project_Created_Date = $Project_Created_Date;
         $this->Project_State = $Project_State;
+      
+        $this->Project_Execution_Record = getProjectExecutionRecords();
     }
     
     public function getButtonStringForProjectState()
@@ -22,6 +37,22 @@
     public function getButtonStringCssClassForProjectState()
     {
         return ($this->Project_State == "CLOSED" ? "btn btn-success" : "btn btn-danger");
+    }
+    
+    public function getProjectExecutionRecords()
+    {
+       $timeRecordList = [];
+       $dbCon = dbConnection::connectToDB();
+       $query_select = sprintf('SELECT * FROM public."Project_Execution_Record" WHERE "Is_Completed" = TRUE AND "Project_ID" = %d ORDER BY "Project_Execution_Record_ID" ASC;', $this->Project_ID);
+       
+       $results = pg_query($dbCon, $query_select) or die('Select query failed: ' . pg_last_error());
+
+        while ($row = pg_fetch_assoc($results))
+          $timeRecordList[] = new Project_Execution_Record($row['Starting_Time_Stamp'], $row['Ending_Time_Stamp']);
+
+        pg_close($dbCon);
+      
+        return $timeRecordList;
     }
     
     public static function fetchAll()
