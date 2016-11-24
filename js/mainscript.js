5562371bf286462b2e6bfb34d536f344574f97b4
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-24 17:51:11 +0400

    Update mainscript.js

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,4 +1,4 @@
-function processAjaxRequest(js_project_id,js_action)
+function processAjaxRequest(js_project_id)
 {
     var tmp_js_project_id = js_project_id.split('_');
     js_project_id = tmp_js_project_id[1];
@@ -10,34 +10,41 @@ function processAjaxRequest(js_project_id,js_action)
     } 
     else 
     {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function()
+        var xmlhttpRecordTime = new XMLHttpRequest();
+        xmlhttpRecordTime.onreadystatechange = function()
         {
-            if (this.readyState == 4 && this.status == 200)
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function()
             {
-                var objArray = JSON.parse(this.responseText);
-                var outputHtml = "";
-                for(var i = 0; i < objArray.length; i++)
+                if (this.readyState == 4 && this.status == 200)
                 {
-                    outputHtml += ('<span class="label label-info">' + objArray[i].Starting_Time_Stamp + ' - ' + objArray[i].Ending_Time_Stamp + '</span><br>');
-                }
+                    var objArray = JSON.parse(this.responseText);
+                    var outputHtml = "";
+                    for(var i = 0; i < objArray.length; i++)
+                    {
+                        outputHtml += ('<span class="label label-info">' + objArray[i].Starting_Time_Stamp + ' - ' + objArray[i].Ending_Time_Stamp + '</span><br>');
+                    }
 
-                document.getElementById("ajaxResponseContainer" + js_project_id).innerHTML = outputHtml;
+                    document.getElementById("ajaxResponseContainer" + js_project_id).innerHTML = outputHtml;
 
-                if(document.getElementById("btnAjax_" + js_project_id).value == "Start working")
-                {
-                    document.getElementById("btnAjax_" + js_project_id).value = "Stop working";
-                    document.getElementById("btnAjax_" + js_project_id).className = "btn btn-danger";
+                    if(document.getElementById("btnAjax_" + js_project_id).value == "Start working")
+                    {
+                        document.getElementById("btnAjax_" + js_project_id).value = "Stop working";
+                        document.getElementById("btnAjax_" + js_project_id).className = "btn btn-danger";
+                    }
+                    else
+                    {
+                        document.getElementById("btnAjax_" + js_project_id).value = "Start working";
+                        document.getElementById("btnAjax_" + js_project_id).className = "btn btn-success"; 
+                    }
                 }
-                else
-                {
-                    document.getElementById("btnAjax_" + js_project_id).value = "Start working";
-                    document.getElementById("btnAjax_" + js_project_id).className = "btn btn-success"; 
-                }
-            }
-        };
+            };
 
-        xmlhttp.open("GET", "restfulapi/getjson.php/getProjectTimeRecords/" + js_project_id, true);
-        xmlhttp.send();
+            xmlhttp.open("GET", "restfulapi/getjson.php/getProjectTimeRecords/" + js_project_id, true);
+            xmlhttp.send();
+        }
+        
+        xmlhttpRecordTime.open("GET", "restfulapi/getjson.php/getProjectTimeRecords/" + js_project_id, true);
+        xmlhttpRecordTime.send();
     }
 }
