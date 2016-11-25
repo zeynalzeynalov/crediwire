86aa4882e8ee34a807984f81866d4c0b336e63c7
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-25 18:54:44 +0400

    Create index.php

---
+++ public_app/index.php
@@ -0,0 +1,22 @@
+<?php
+    require_once('config/configuration.php');
+    require_once('includes/dbconnection.php');
+    
+    $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+
+    $CONTROLLER = strtoupper(preg_replace('/[^a-z0-9_]+/i','',array_shift($request)));
+    $ACTION = array_shift($request)+0;
+
+    if (isset($CONTROLLER) && isset($ACTION))
+    {
+        $controller = $CONTROLLER;
+        $action     = $ACTION;
+    }
+    else
+    {
+        $controller = 'projects';
+        $action     = 'main';
+    }
+    
+    require_once('views/template.php');
+?>
