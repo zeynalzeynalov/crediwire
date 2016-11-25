59acdd6c0d7117e1415872d909de97a37bd96ae6
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-25 20:05:46 +0400

    Update help.php

--- views/help.php
+++ views/help.php
@@ -1,13 +1,25 @@
 <div class="container">
   <div class="panel-group">
 
-    <div class="panel panel-danger">
+    <div class="panel panel-primary">
       <div class="panel-heading">
-        <h3 class="panel-title">Error page</h3>
+        <h3 class="panel-title">Technical documentation of developed Project Time Management App.</h3>
       </div>
       <div class="panel-body">
-        Technical documentation of developed Project Time Management App.
         
+      DATABASE:
+      Server:     PostgreSQL VERSION 9.5.5 
+      Host:       ec2-54-217-213-203.eu-west-1.compute.amazonaws.com
+      Database:   dcreosvnjcrc0o
+      User:       phueqmfbfolrhb
+      Port:       5432
+      Password:   A2vfu6M2r5LhhxAdBA63VWCi6U
+      URI:        postgres://phueqmfbfolrhb:A2vfu6M2r5LhhxAdBA63VWCi6U@ec2-54-217-213-203.eu-west-1.compute.amazonaws.com:5432/dcreosvnjcrc0oHeroku
+      CLI:        heroku pg:psql DATABASE_URL --app cready  
+        
+      Tables:
+        project
+        project_execution_record
         
       </div>
     </div>
