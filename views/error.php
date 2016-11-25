68777d57533eaada123b46dfc570113be9b06775
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-25 20:15:18 +0400

    Create error.php

---
+++ views/error.php
@@ -0,0 +1,14 @@
+<div class="container">
+  <div class="panel-group">
+
+    <div class="panel panel-danger">
+      <div class="panel-heading">
+        <h3 class="panel-title">Error page</h3>
+      </div>
+      <div class="panel-body">
+        There are some technical problems.
+      </div>
+    </div>
+
+  </div>
+</div>  
