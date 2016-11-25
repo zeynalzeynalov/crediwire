da0d88a8a9a83dbf35436fb3b723698cea603193
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-25 19:56:46 +0400

    Update index.php

--- public_app/index.php
+++ public_app/index.php
@@ -3,16 +3,8 @@
     require_once(dirname(dirname(__FILE__)).'/includes/dbconnection.php');
     
     $_request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
-
-    echo '[0]'.$_request[0];
-    echo '[1]'.$_request[1];
-
-    echo '<br>server url:'.$_SERVER['PATH_INFO'];
-
-    $_CONTROLLER = strtoupper(preg_replace('/[^a-z0-9_]+/i','',array_shift($_request)));
-    $_ACTION = array_shift($_request)+0;
-
-    echo $_CONTROLLER.'-'.$_ACTION;
+    $_CONTROLLER = $_request[0];
+    $_ACTION = $_request[1];
 
     if (isset($_CONTROLLER) && isset($_ACTION))
     {
