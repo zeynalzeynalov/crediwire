d25d2115ba46176cc3488a05571cadc9ba234cc4
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-25 17:11:00 +0400

    Update main.php

--- views/projects/main.php
+++ views/projects/main.php
@@ -6,28 +6,28 @@
   <div class="panel-group">
       <div class="panel panel-primary">
       <div class="panel-heading">
-      <h3 class="panel-title">Project Name: <?= $project->Project_Title ?></h3>
+      <h3 class="panel-title">Project Name: <?= $project->project_title ?></h3>
       </div>
       <div class="panel-body">
         
-      Project created on: <?= $project->Project_Created_Date ?>
+      Project created on: <?= $project->project_created_date ?>
         <br>
-      <input class="<?= $project->getButtonStringCssClassForProjectState() ?>" type="button" id="btnAjax_<?= $project->Project_id ?>" value="<?= $project->getButtonStringForProjectState() ?>" onclick="processAjaxRequest(this.id)">
+      <input class="<?= $project->getButtonStringCssClassForProjectState() ?>" type="button" id="btnAjax_<?= $project->project_id ?>" value="<?= $project->getButtonStringForProjectState() ?>" onclick="processAjaxRequest(this.id)">
 
       <br />
       <br />
-      <a class="btn btn-default btn-sm" role="button" data-toggle="collapse" href="#collapseExample_<?= $project->Project_id ?>" aria-expanded="true" aria-controls="collapseExample">
+      <a class="btn btn-default btn-sm" role="button" data-toggle="collapse" href="#collapseExample_<?= $project->project_id ?>" aria-expanded="true" aria-controls="collapseExample">
       Show/hide
       </a>
         
 
-      <div class="collapse" id="collapseExample_<?= $project->Project_id ?>">
+      <div class="collapse" id="collapseExample_<?= $project->project_id ?>">
       <div class="well">
-      <p> <span id="ajaxResponseContainer<?= $project->Project_id ?>">
+      <p> <span id="ajaxResponseContainer<?= $project->project_id ?>">
         
         <?php foreach ($project->Project_Execution_Record as $timeRecord): ?>
             
-        <span class="label label-info"><?= $timeRecord->Starting_Time_Stamp ?> - <?= $timeRecord->Ending_Time_Stamp ?> Total spent time = <?= $timeRecord->Time_Diff_Text ?> </span><br>
+        <span class="label label-info"><?= $timeRecord->starting_time_stamp ?> - <?= $timeRecord->ending_time_stamp ?> Total spent time = <?= $timeRecord->time_diff_text ?> </span><br>
                         
         <?php endforeach; ?>
         
