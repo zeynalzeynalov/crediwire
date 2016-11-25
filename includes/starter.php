9eb3af8914945dc82d1c11490382bcd494c203f6
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-25 19:15:46 +0400

    Update starter.php

--- includes/starter.php
+++ includes/starter.php
@@ -1,30 +1,27 @@
 <?php
-  function call($controller, $action)
-  {
-    require_once(dirname(dirname(__FILE__)).'/controllers/' . $controller . '_controller.php');
+      function start($controller, $action)
+      {
+            require_once(dirname(dirname(__FILE__)).'/controllers/' . $controller . '_controller.php');
 
-    switch($controller) {
-      case 'projects':
-        // we need the model to query the database later in the controller
-        require_once('models/project.php');
-        $controller = new ProjectsController();
-      break;
-    }
+            switch($controller) {
+              case 'projects':
+                  require_once(dirname(dirname(__FILE__)).'/models/project.php');
+                  $controller = new ProjectsController();
+              break;
+            }
 
-    $controller->{ $action }();
-  }
+            $controller->{ $action }();
+      }
 
-  // we're adding an entry for the new controller and its actions
-  $controllers = array('projects' => ['main', 'error'],
-                       'posts' => ['index', 'show']);
+      $controllerList = array('projects' => ['main', 'error'], 'help' => ['help', 'error']);
 
-  if (array_key_exists($controller, $controllers)) {
-    if (in_array($action, $controllers[$controller])) {
-      call($controller, $action);
-    } else {
-      call('projects', 'error');
-    }
-  } else {
-    call('projects', 'error');
-  }
+      if (array_key_exists($controller, $controllerList))
+      {
+          if (in_array($action, $controllerList[$controller]))
+              start($controller, $action);
+          else
+              start('projects', 'error');
+      } 
+      else
+          start('projects', 'error');
 ?>
