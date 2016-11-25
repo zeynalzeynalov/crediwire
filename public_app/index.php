3d4e3ef0f5ad336f14cf091f963d168a20539f40
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-25 19:49:50 +0400

    Update index.php

--- public_app/index.php
+++ public_app/index.php
@@ -2,15 +2,15 @@
     require_once(dirname(dirname(__FILE__)).'/config/configuration.php');
     require_once(dirname(dirname(__FILE__)).'/includes/dbconnection.php');
     
-    $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+    $_request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
 
     echo $request[0];
     echo $request[1];
 
     echo $_SERVER['PATH_INFO'];
 
-    $_CONTROLLER = strtoupper(preg_replace('/[^a-z0-9_]+/i','',array_shift($request)));
-    $_ACTION = array_shift($request)+0;
+    $_CONTROLLER = strtoupper(preg_replace('/[^a-z0-9_]+/i','',array_shift($_request)));
+    $_ACTION = array_shift($_request)+0;
 
     echo $CONTROLLER.'-'.$ACTION;
 
