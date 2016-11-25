bdf9ff268b0d080f757b757ceec1f789400df2da
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-25 19:25:47 +0400

    Update error.php

--- views/projects/error.php
+++ views/projects/error.php
@@ -1,3 +1,8 @@
-<p>Oops, this is the error page.</p>
-
-<p>Looks like something went wrong.</p>
+<div class="panel panel-danger">
+  <div class="panel-heading">
+    <h3 class="panel-title">Error page</h3>
+  </div>
+  <div class="panel-body">
+    There are some technical problems.
+  </div>
+</div>
