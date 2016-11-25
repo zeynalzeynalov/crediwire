f4b26f4fc198334432d59337c7c3584059ab5130
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-25 19:33:46 +0400

    Update starter.php

--- includes/starter.php
+++ includes/starter.php
@@ -2,7 +2,9 @@
       function start($controller, $action)
       {
             require_once(dirname(dirname(__FILE__)).'/controllers/' . $controller . '_controller.php');
-
+            echo $controller;
+            echo $action;
+            
             switch($controller) {
               case 'projects':
                   require_once(dirname(dirname(__FILE__)).'/models/project.php');
