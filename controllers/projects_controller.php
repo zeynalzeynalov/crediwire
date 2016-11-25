f358754a313787f10fb12e407dd14231ea867692
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-25 19:18:41 +0400

    Update projects_controller.php

--- controllers/projects_controller.php
+++ controllers/projects_controller.php
@@ -1,30 +1,15 @@
 <?php
-  class ProjectsController {
-    public function main() {
-      $first_name = 'Jon';
-      $last_name  = 'Snow';
-      
-      $projects = Project::fetchAll();
-      
-      require_once('views/projects/main.php');
-    }
+      class ProjectsController
+      {
+            public function main()
+            {
+                  $projects = Project::fetchAll();
+                  require_once(dirname(dirname(__FILE__)).'/views/projects/main.php');
+            }
 
-    public function error() {
-      require_once('views/projects/error.php');
-    }
-    
-    public function show() {
-      // we expect a url of form ?controller=posts&action=show&id=x
-      // without an id we just redirect to the error page as we need the post id to find it in the database
-      if (!isset($_GET['id']))
-        return call('pages', 'error');
-
-      // we use the given id to get the right post
-      $post = Post::find($_GET['id']);
-      require_once('views/posts/show.php');
-    }
-    
-    
-    
-  }
+            public function error()
+            {
+                  require_once(dirname(dirname(__FILE__)).'/views/projects/error.php');
+            } 
+      }
 ?>
