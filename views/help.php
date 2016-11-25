e664dfa50873bc4e112378deb3b302d62dcdb576
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-25 19:59:35 +0400

    Update help.php

--- views/help.php
+++ views/help.php
@@ -1,1 +1,16 @@
+<div class="container">
+  <div class="panel-group">
 
+    <div class="panel panel-danger">
+      <div class="panel-heading">
+        <h3 class="panel-title">Error page</h3>
+      </div>
+      <div class="panel-body">
+        Technical documentation of developed Project Time Management App.
+        
+        
+      </div>
+    </div>
+
+  </div>
+</div>  
