1afe40840bac76f64c38f8d1f6a008817b3c3067
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-25 20:12:23 +0400

    Update help_controller.php

--- controllers/help_controller.php
+++ controllers/help_controller.php
@@ -1,1 +1,14 @@
-
+<?php
+      class HelpController
+      {
+            public function main()
+            {
+                  require_once(dirname(dirname(__FILE__)).'/views/help/main.php');
+            }
+        
+            public function error()
+            {
+                  require_once(dirname(dirname(__FILE__)).'/views/help/error.php');
+            } 
+      }
+?>
