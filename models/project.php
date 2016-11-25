c098984b5adbc765b7300514dad523eca2f120a1
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-25 16:51:51 +0400

    Update project.php

--- models/project.php
+++ models/project.php
@@ -16,15 +16,15 @@
 
     class Project {
 
-    public $Project_ID;
+    public $Project_id;
     public $Project_Title;
     public $Project_Created_Date;
     public $Project_State;
     public $Project_Execution_Record;
         
-    public function __construct($Project_ID, $Project_Title, $Project_Created_Date, $Project_State, $timeRecordList)
+    public function __construct($Project_id, $Project_Title, $Project_Created_Date, $Project_State, $timeRecordList)
     {
-        $this->Project_ID = $Project_ID;
+        $this->Project_id = $Project_id;
         $this->Project_Title = $Project_Title;
         $this->Project_Created_Date = $Project_Created_Date;
         $this->Project_State = $Project_State;
@@ -46,15 +46,15 @@
     {
         $projectList = [];
         $dbCon = dbConnection::connectToDB();
-        $results = pg_query($dbCon, 'SELECT P.*, public.Check_Project_State(P.Project_ID) Project_State
-            FROM public.Project P ORDER BY P.Project_ID ASC') or die('Select query failed: ' . pg_last_error());
+        $results = pg_query($dbCon, 'SELECT P.*, public.Check_Project_State(P.Project_id) Project_State
+            FROM public.Project P ORDER BY P.Project_id ASC') or die('Select query failed: ' . pg_last_error());
 
         while ($row = pg_fetch_assoc($results))
         {
             
             $timeRecordList = [];
             $dbCon = dbConnection::connectToDB();
-            $query_select = sprintf("SELECT *, TO_CHAR(interval '1 second' * final_execution_time, 'HH24:MI:SS') Time_Diff_Text FROM public.project_execution_Record WHERE Is_Completed = TRUE AND Project_ID = %d ORDER BY Project_Execution_Record_ID ASC;", $row['project_id']);
+            $query_select = sprintf("SELECT *, TO_CHAR(interval '1 second' * final_execution_time, 'HH24:MI:SS') Time_Diff_Text FROM public.project_execution_Record WHERE Is_Completed = TRUE AND Project_id = %d ORDER BY Project_Execution_Record_id ASC;", $row['project_id']);
 
             $r = pg_query($dbCon, $query_select) or die('Select query failed: ' . pg_last_error());
 
@@ -63,7 +63,7 @@
                 $timeRecordList[] = new Project_Execution_Record($w['Starting_Time_Stamp'], $w['Ending_Time_Stamp'], $w['Time_Diff_Text']);
             }
 
-            $projectList[] = new Project($row['Project_ID'], $row['Project_Title'], $row['Project_Created_Date'], $row['Project_State'], $timeRecordList); 
+            $projectList[] = new Project($row['Project_id'], $row['Project_Title'], $row['Project_Created_Date'], $row['Project_State'], $timeRecordList); 
         }
         pg_close($dbCon);
         return $projectList;
