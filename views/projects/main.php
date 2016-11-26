6a5221bea75c2cc7263d014fdbb10fd96b81bd38
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-26 18:05:08 +0400

    Update main.php

--- views/projects/main.php
+++ views/projects/main.php
@@ -1,7 +1,12 @@
-<center><h4>Let's start completing waiting projects!</h4></center>
+<center>
+          <div class="alert alert-info" role="alert" id="message-alert-box">
+          <span class="glyphicon glyphicon glyphicon-ok" aria-hidden="true"></span>
+          <span class="sr-only">Error:</span>
+          <span id="save-result"><h4>Let's start completing waiting projects!</h4></span>
+        </div>
+ </center>
 
 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo">+ New Project</button>
-
 
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
   <div class="modal-dialog" role="document">
@@ -16,23 +21,15 @@
             <label for="tb-project-title" class="control-label">Project title:</label>
             <input type="text" class="form-control" id="tb-project-title">
           </div>
-          <div class="alert alert-info" role="alert">
-          <span class="glyphicon glyphicon glyphicon-ok" aria-hidden="true"></span>
-          <span class="sr-only">Error:</span>
-          <span id="save-result">New project added!</span>
-        </div>
+
         </form>
         
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary" id="btn-save-project">Save</button>
         
-          <button type="button" id="myButton" data-loading-text="Loading..." class="btn btn-primary" autocomplete="off">
-            Loading state
-          </button>
         
-        <input class="btn btn-success" type="button" id="btndummy" value="add project" onclick="processAjaxRequestAddNewProject('new project')">
+        <input class="btn btn-success" type="button" id="btnAddProject" data-dismiss="modal" value="add project" onclick="processAjaxRequestAddNewProject('new project')">
 
       </div>
     </div>
