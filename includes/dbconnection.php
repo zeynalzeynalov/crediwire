697a33aa731b6d1ad79735a7723d04ed93e1672a
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-26 21:10:46 +0400

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -7,7 +7,7 @@
     {    
         $dbConnection = pg_connect(sprintf("host=%s user=%s password=%s dbname=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME))
           
-        or die ("Could not connect to server\n");;
+        or die ("Could not connect to database server\n");;
          
         return $dbConnection;
     }
