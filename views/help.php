ea56354883a2b48e211c0a3fd85731e8216af2ee
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-26 14:30:21 +0400

    Update help.php

--- views/help.php
+++ views/help.php
@@ -16,11 +16,28 @@
       Port:       5432<br>
       Password:   A2vfu6M2r5LhhxAdBA63VWCi6U<br>
       URI:        postgres://phueqmfbfolrhb:A2vfu6M2r5LhhxAdBA63VWCi6U@ec2-54-217-213-203.eu-west-1.compute.amazonaws.com:5432/dcreosvnjcrc0oHeroku<br>
-      CLI:        heroku pg:psql DATABASE_URL --app cready<br>
+      CLI:        heroku pg:psql DATABASE_URL --app cready<br><br><br>
         
-      Tables:<br>
-        project<br>
-        project_execution_record<br>
+      TABLES:<br><br>
+        <b>- project</b><br>
+        <span>Columns:
+              <b>project_id integer (PK)</b>,
+              project_title character varying,
+              project_created_date date,
+        </span>
+        
+        <b>- project_execution_record</b><br>
+        <span>Columns:
+              <b>project_execution_record_id integer (PK)</b>,
+
+              starting_time_stamp timestamp,
+              ending_time_stamp timestamp,
+              is_completed boolean,
+              project_id integer (FK),
+              final_execution_time bigint,
+        </span>
+        
+        <br>
       </p>
         
       </div>
