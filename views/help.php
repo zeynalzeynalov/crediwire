a613b60dfae51b757201a08191566da292e4a6e0
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-26 15:05:49 +0400

    Update help.php

--- views/help.php
+++ views/help.php
@@ -17,8 +17,7 @@
       Password:   A2vfu6M2r5LhhxAdBA63VWCi6U
       URI:        postgres://phueqmfbfolrhb:A2vfu6M2r5LhhxAdBA63VWCi6U@ec2-54-217-213-203.eu-west-1.compute.amazonaws.com:5432/dcreosvnjcrc0oHeroku
       CLI:        heroku pg:psql DATABASE_URL --app cready
-      </pre>
-      
+
       <pre>
       TABLES [2]:
         <b>- project</b>
@@ -37,7 +36,8 @@
                   project_id integer (FK),
                   final_execution_time bigint,
             </span>
-        
+        </pre>
+        <pre>
         FUNCTIONS [2]:
         <b>check_project_state( project_id integer )</b>
               Check that indicated project is Closed or Open.
@@ -45,7 +45,46 @@
         <b>get_timestamp_diff( project_id integer )</b>
               Get time interval between ending and starting time stamps in format of 'HH24:MI:SS'.
         </pre>
+
+      </pre>
       
+      <pre>
+      APPLICATION:
+        Language:     PHP
+        Markup:       CSS, Bootstrap v3.3.7
+        Frontend:     Javascript, AJAX, Json
+        Pattern:      MVC
+        APIs design:  RESTful
+        
+        <b>Directory structure:</b>
+            root/
+                config/
+                      configuration.php
+                controllers/
+                      help_controller.php
+                      projects_controller.php
+                includes/
+                      dbconnection.php
+                      starter.php
+                models/
+                      project.php
+                public_app/
+                      js/
+                          mainscript.js
+                      .htaccess
+                      index.php
+                restfulapi/
+                      getjson.php
+                      performaction.php
+                views/
+                      projects/
+                          main.php
+                          error.php
+                      template.php
+                      help.php
+                      error.php
+                .htaccess
+                index.php
         
       </div>
     </div>
