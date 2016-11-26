3b047ae13bf8a05744a578473a582682116a4f4d
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-26 18:53:43 +0400

    Update help.php

--- views/help.php
+++ views/help.php
@@ -85,6 +85,123 @@
         <b> get_timestamp_diff( 	start timestamp, end timestamp) RETURNS character varying</b>
               Get time interval between ending and starting time stamps in format of 'HH24:MI:SS'.
         </pre>
+        
+        <pre>
+        <b>Database SQL Script:</b>
+            
+            -- Table: public.project
+            -- DROP TABLE public.project;
+
+            CREATE TABLE public.project
+            (
+                project_id integer NOT NULL DEFAULT nextval('"Project_Project_ID_seq"'::regclass),
+                project_title character varying(30) COLLATE pg_catalog."default" NOT NULL,
+                project_created_date date NOT NULL,
+                CONSTRAINT "Project_pkey" PRIMARY KEY (project_id)
+            )
+            WITH (
+                OIDS = FALSE
+            )
+            TABLESPACE pg_default;
+
+            ALTER TABLE public.project
+                OWNER to phueqmfbfolrhb;
+            COMMENT ON TABLE public.project
+                IS 'Project list';
+                
+            
+            
+            -- Table: public.project_execution_record
+            -- DROP TABLE public.project_execution_record;
+
+            CREATE TABLE public.project_execution_record
+            (
+                project_execution_record_id integer NOT NULL DEFAULT nextval('"Project_Execution_Record_Project_Execution_Record_ID_seq"'::regclass),
+                starting_time_stamp timestamp without time zone NOT NULL,
+                ending_time_stamp timestamp without time zone,
+                is_completed boolean NOT NULL DEFAULT false,
+                project_id integer NOT NULL,
+                final_execution_time bigint,
+                CONSTRAINT "Project_Execution_Record_pkey" PRIMARY KEY (project_execution_record_id),
+                CONSTRAINT "Project_Execution_Record_Project_ID_fkey" FOREIGN KEY (project_id)
+                    REFERENCES public.project (project_id) MATCH SIMPLE
+                    ON UPDATE NO ACTION
+                    ON DELETE NO ACTION
+            )
+            WITH (
+                OIDS = FALSE
+            )
+            TABLESPACE pg_default;
+
+            ALTER TABLE public.project_execution_record
+                OWNER to phueqmfbfolrhb;
+        
+        
+        
+        -- FUNCTION: public.check_project_state(integer)
+        -- DROP FUNCTION public.check_project_state(integer);
+
+        CREATE OR REPLACE FUNCTION public.check_project_state(
+          project_id integer)
+            RETURNS character varying
+            LANGUAGE 'sql'
+            COST 100.0
+            VOLATILE NOT LEAKPROOF 
+        AS $function$
+
+        with myResult as
+        (
+            SELECT
+            CASE WHEN coalesce(Is_Completed, FALSE) = FALSE 
+            THEN 'OPEN' 
+            ELSE 'CLOSED' END  
+            Project_State
+            FROM public.Project_Execution_Record
+            WHERE Project_ID = $1
+            ORDER BY Project_Execution_Record_ID desc limit 1
+        )
+
+        select * from myResult
+        union
+        select 'CLOSED' from Project
+        where (select count(*) from myResult) = 0
+
+        $function$;
+
+        ALTER FUNCTION public.check_project_state(integer)
+            OWNER TO phueqmfbfolrhb;
+
+        COMMENT ON FUNCTION public.check_project_state(integer)
+            IS 'Check project state: running / stopped';
+
+        
+        -- FUNCTION: public.get_timestamp_diff(timestamp without time zone, timestamp without time zone)
+        -- DROP FUNCTION public.get_timestamp_diff(timestamp without time zone, timestamp without time zone);
+
+        CREATE OR REPLACE FUNCTION public.get_timestamp_diff(
+          start timestamp without time zone,
+          "end" timestamp without time zone)
+            RETURNS character varying
+            LANGUAGE 'sql'
+            COST 100.0
+            VOLATILE NOT LEAKPROOF 
+        AS $function$
+
+        SELECT
+          TO_CHAR(concat(EXTRACT(EPOCH FROM ($2 - $1)) , ' second')::interval, 'HH24:MI:SS') TimeDiff
+        FROM
+          public.project_execution_record;
+
+        $function$;
+
+        ALTER FUNCTION public.get_timestamp_diff(timestamp without time zone, timestamp without time zone)
+            OWNER TO phueqmfbfolrhb;
+
+        COMMENT ON FUNCTION public.get_timestamp_diff(timestamp without time zone, timestamp without time zone)
+            IS 'Get timestamp diff in hours and minutes';
+
+        
+        </pre>
       </pre>
         
       </div>
