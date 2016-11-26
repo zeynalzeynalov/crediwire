2d9f7f8e6f36c3c01fd1ac8bf701f76b3a91a059
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-26 20:02:38 +0400

    Update getjson.php

--- restfulapi/getjson.php
+++ restfulapi/getjson.php
@@ -1,9 +1,9 @@
 <?php
-	/*
+	
 	header("Access-Control-Allow-Orgin: *");
 	header("Access-Control-Allow-Methods: *");
 	header("Content-Type: application/json");
-	*/
+	
 	require_once(dirname(dirname(__FILE__)).'/config/configuration.php');
 	require_once(dirname(dirname(__FILE__)).'/includes/dbconnection.php');
 
