6b771b674a58f61f9fbc7e7d17d78d00fc0c4810
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-26 20:36:30 +0400

    Update template.php

--- views/template.php
+++ views/template.php
@@ -1,25 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>CrediWire coding assignment. [Zeynal Zeynalov]</title>
+		<title>CrediWire coding assignment. [Zeynal Zeynalov]</title>
 
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<!-- Bootstrap -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-    
-    <script type="text/javascript" src="js/mainscript.js"></script>
-    
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>    
-  </head>
-  <body>
-    
+		
+		<script type="text/javascript" src="js/mainscript.js"></script>
+		
+		<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>    
+	</head>
+	<body>
       <nav class="navbar navbar-default">
       <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
@@ -55,18 +54,18 @@
       </div><!-- /.container-fluid -->
     </nav>
     
-    <header>
+	<header>
 
-    </header>
-    
-    <form> 
-      <?php require_once(dirname(dirname(__FILE__)).'/includes/starter.php'); ?>
-    </form>
-    
-    <footer>
-      <center>CrediWire coding assignment. [Zeynal Zeynalov]</center>
-      <br>
-    </footer>
+	</header>
+
+	<form> 
+		<?php require_once(dirname(dirname(__FILE__)).'/includes/starter.php'); ?>
+	</form>
+
+	<footer>
+		<center>CrediWire coding assignment. [Zeynal Zeynalov]</center>
+	<br>
+	</footer>
     
     </body>
 </html>
