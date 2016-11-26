81812b4b184dbe8677d0f574b5ace33ca5adfa89
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-26 21:41:38 +0400

    Update help.php

--- views/help.php
+++ views/help.php
@@ -9,9 +9,15 @@
 
       <pre>
       APPLICATION:
+      
+        This demo is working version of coding assigment. I tried to use Ajax requests in order to prevent postbacks to the server side
+        if there is a button click. And also design is responsive that it is adaptive to mobile devices.  
+        API module of application also useful for integration posibilities (please use top API menu to test demo Json API).
+        Pattern design is MVC.
+      
         Language:     PHP
         Markup:       HTML, CSS, <a href="http://getbootstrap.com/" target="_blank">Bootstrap 3</a>
-        Frontend:     Javascript, AJAX, Json
+        Frontend:     Javascript, AJAX, Json, Google charts
         Pattern:      MVC
         APIs design:  RESTful
         Development enviroment: <a href="https://www.heroku.com/" target="_blank">Heroku: Cloud Application Platform</a>
