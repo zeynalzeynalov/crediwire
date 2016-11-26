ad17d1693a6eb18eb46a122dd99cdfc28d51d128
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-26 18:39:24 +0400

    Update template.php

--- views/template.php
+++ views/template.php
@@ -36,17 +36,18 @@
           <ul class="nav navbar-nav">
             <!--li class="active"><a href="#">Dummy<span class="sr-only">(current)</span></a></li-->
             <li class="dropdown">
-          <a href="#" class="btn btn-info dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">API<span class="caret"></span></a>
+          <a href="#" class="btn-default dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">API DEMO<span class="caret"></span></a>
           <ul class="dropdown-menu">
             <li><a href="https://crediwire.herokuapp.com/restfulapi/getjson.php/getProjectDetails/1" target="_blank">API - Get Project Details (Project ID = 1)</a></li>
             <li><a href="https://crediwire.herokuapp.com/restfulapi/getjson.php/getProjectTimeRecords/1" target="_blank">API - Get Project Time Records (Project ID = 1)</a></li>
+            <li><a href="https://crediwire.herokuapp.com/restfulapi/performaction.php/addnewproject/Test_Project" target="_blank">API - Add New Project (Title: Test Project)</a></li>            
           </ul>
         </li>
           </ul>
 
           <ul class="nav navbar-nav navbar-right">
             <li><a href="https://crediwire.herokuapp.com/help/main">Help</a></li>
-            <li><a href="#">Welcome, <b>Bob!</b></a></li>
+            <li><a href="https://crediwire.herokuapp.com/">Welcome, <b>Bob!</b></a></li>
           </ul>
         </div><!-- /.navbar-collapse -->
       </div><!-- /.container-fluid -->
