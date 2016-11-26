bbd419dd99be4620d408dfe210cbbe96b4cc4cc5
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2016-11-26 20:34:41 +0400

    Update mainscript.js

--- public_app/js/mainscript.js
+++ public_app/js/mainscript.js
@@ -1,3 +1,25 @@
+// Load the Visualization API and the piechart package.
+google.charts.load('current', {'packages':['corechart']});
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart()
+{
+    var jsonData = $.ajax({
+    url: "restfulapi/getjson.php/getTotalProjectDurations/index.php",
+    dataType: "json",
+    async: false
+    }).responseText;
+
+    // Create our data table out of JSON data loaded from server.
+    var data = new google.visualization.DataTable(jsonData);
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    chart.draw(data, {width: 400, height: 240});
+}
+
 function processAjaxRequest(js_project_id)
 {
     var tmp_js_project_id = js_project_id.split('_');
