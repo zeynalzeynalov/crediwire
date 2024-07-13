f4f026e6133c7cda9abcd3e20f36e9665fd1902e
zeynalzeynalov <zeynalov.zeynal@gmail.com>
2024-07-14 00:47:39 +0400

    Update configuration.php

--- config/configuration.php
+++ config/configuration.php
@@ -1,9 +1,9 @@
 <?php
 
 // PostgreSQL Database connection settings
-define('DB_HOST', 'ec2-54-217-213-203.eu-west-1.compute.amazonaws.com');
-define('DB_USER', 'phueqmfbfolrhb');
-define('DB_PASSWORD', 'A2vfu6M2r5LhhxAdBA63VWCi6U');
-define('DB_NAME', 'dcreosvnjcrc0o');
+define('DB_HOST', 'test');
+define('DB_USER', 'test');
+define('DB_PASSWORD', 'test');
+define('DB_NAME', 'test');
 
 ?>
